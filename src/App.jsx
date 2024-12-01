3785809a9d574d2d476059ba125aee3ee9fdc74b
hamidr-source <hr454837@gmail.com>
2024-12-01 17:48:01 +0330

    Update project srtucture

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-green-300 flex justify-center items-center text-2xl">
+      Hello World
+    </div>
+  );
+};
+
+export default App;
