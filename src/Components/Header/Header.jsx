a8d9610ef325a4583423f39bae6598572de6a41c
hamidr-source <hr454837@gmail.com>
2024-12-02 15:09:38 +0330

    Update navigation bar styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -39,13 +39,29 @@ const Header = () => {
       <div
         className={
           mobileMenu
-            ? "absolute top-0 right-0 bg-white shadow-lg w-3/4 h-full z-20 flex"
+            ? "absolute top-0 right-0 bg-white shadow-lg w-3/4 h-full z-20 flex flex-col gap-8"
             : "hidden"
         }
       >
-        <button onClick={() => setMobileMenu(false)}>
-        <i className="fa-solid fa-x"></i>
-        </button>
+        <div className="mt-3 ml-3">
+          <button onClick={() => setMobileMenu(false)}>
+            <i className="fa-solid fa-x text-red-600 font-bold"></i>
+          </button>
+        </div>
+        <div className="flex flex-col gap-6 h-1/3">
+          <p className="pl-6 h-1/4 w-full text-gray-700 flex gap-2 items-center hover:text-white hover:bg-gray-500">
+            <i className="fa-solid fa-house"></i>Home
+          </p>
+          <p className="pl-6 h-1/4 w-full text-gray-700 flex gap-2 items-center hover:text-white hover:bg-gray-500">
+            <i className="fa-solid fa-info"></i>About
+          </p>
+          <p className="pl-6 h-1/4 w-full text-gray-700 flex gap-2 items-center hover:text-white hover:bg-gray-500">
+            <i className="fa-solid fa-address-book"></i>Contact
+          </p>
+          <p className="pl-6 h-1/4 w-full text-gray-700 flex gap-2 items-center hover:text-white hover:bg-gray-500">
+            <i className="fa-solid fa-user"></i>Login
+          </p>
+        </div>
       </div>
     </>
   );
