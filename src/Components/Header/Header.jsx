3806ca7c818463123a2f10fa44034848d52480d7
hamidr-source <hr454837@gmail.com>
2025-01-08 01:06:50 +0330

    Refactor the sideBar code

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Search from "./Search";
 import SideBar from "./SideBar";
--- src/Components/Header/SideBar.jsx
+++ src/Components/Header/SideBar.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ThemeController from "../ThemeController/ThemeController";
 
 const SideBar = () => {
+  const sideBarNavigation = [
+    { title: "Home", icon: "fa-solid fa-house" },
+    { title: "Contact", icon: "fa-solid fa-address-book" },
+    { title: "About", icon: "fa-solid fa-info" },
+    { title: "Login", icon: "fa-solid fa-user" },
+  ];
+
+  const [activeItem, setActiveItem] = useState(null);
+
   return (
     <>
-      <div className="flex flex-col gap-6 h-1/3">
-        <p className="pl-6 h-1/4 w-full flex gap-2 items-center">
-          <i className="fa-solid fa-house"></i>Home
-        </p>
-        <p className="pl-6 h-1/4 w-full flex gap-2 items-center">
-          <i className="fa-solid fa-info"></i>About
-        </p>
-        <p className="pl-6 h-1/4 w-full flex gap-2 items-center">
-          <i className="fa-solid fa-address-book"></i>Contact
-        </p>
-        <p className="pl-6 h-1/4 w-full flex gap-2 items-center">
-          <i className="fa-solid fa-user"></i>Login
-        </p>
+      <div className="flex flex-col gap-6 h-1/3 group">
+        {sideBarNavigation.map((item, index) => (
+          <p
+            key={index}
+            className={`pl-6 h-1/4 w-full flex gap-2 items-center 
+              ${activeItem === index ? "text-interactive-active text-white" : ""} 
+              `}
+            onMouseEnter={() => setActiveItem(index)}
+            onMouseLeave={() => setActiveItem(null)}
+          >
+            <i className={item.icon}></i>
+            {item.title}
+          </p>
+        ))}
       </div>
       <div className="absolute bottom-4 right-4">
         <ThemeController />
