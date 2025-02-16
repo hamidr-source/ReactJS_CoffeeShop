d04a99d4135a44fea08fd33daee29ad604d8d861
hamidr-source <hr454837@gmail.com>
2025-02-16 17:13:04 +0330

    Update product page style

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -12,7 +12,7 @@ const ProductPage = () => {
         const res = await axios.get(
           `https://fake-coffee-api.vercel.app/api/${params.id}`
         );
-        setProduct(res.data[0])
+        setProduct(res.data[0]);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
@@ -21,7 +21,16 @@ const ProductPage = () => {
     fetchProduct();
   }, []);
 
-  return <div>{product.name}</div>;
+  return (
+    <div className="h-screen w-screen container text-xl">
+      <img src={product.image_url} className="h-1/3 w-full mt-4" />
+      <div className="flex justify-evenly">
+        <p className="font-bold">{product.name}</p>
+        <p className="text-green-500 font-bold">{product.price} $</p>
+      </div>
+      <p className="px-4 mt-4">{product.description}</p>
+    </div>
+  );
 };
 
 export default ProductPage;
