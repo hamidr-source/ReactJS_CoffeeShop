06426a8127703b3a77359ad95094db43510c355c
hamidr-source <hr454837@gmail.com>
2025-02-26 14:45:54 +0330

    Add dropdown for product page

---
+++ src/Pages/ProductPage/Dropdown.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const Dropdown = ({ grindOption }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen((prevIndex) => !prevIndex);
+  };
+  console.log(grindOption);
+  return (
+    <div className="w-full h-fit max-w-[400px]">
+      <button
+        onClick={toggleAccordion}
+        className="w-2/4 p-4 flex justify-between items-center"
+      >
+        Grind Option {" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`h-5 w-5 transition-transform ${
+            isOpen ? "transform rotate-180" : ""
+          }`}
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+      {isOpen && (
+        <div className="border-b last:border-b-0">
+          {grindOption.map((options, index) => (
+            <p className="p-4 bg-gray-100" key={index}>
+              {options}
+            </p>
+          ))}
+          <p className="p-4 bg-gray-100">None</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Dropdown from "./Dropdown";
 
 const ProductPage = () => {
   const [product, setProduct] = useState([]);
@@ -12,6 +13,7 @@ const ProductPage = () => {
         const res = await axios.get(
           `https://fake-coffee-api.vercel.app/api/${params.id}`
         );
+        console.log(res.data[0]);
         setProduct(res.data[0]);
       } catch (error) {
         console.error("Error fetching products:", error);
@@ -20,15 +22,18 @@ const ProductPage = () => {
 
     fetchProduct();
   }, []);
+  console.log(product);
 
   return (
     <div className="h-screen w-screen container text-xl">
       <img src={product.image_url} className="h-1/3 w-full mt-4" />
-      <div className="flex justify-evenly">
+      <div className="flex justify-between p-4">
         <p className="font-bold">{product.name}</p>
         <p className="text-green-500 font-bold">{product.price} $</p>
       </div>
+      <p className="px-4 mt-4">Flavor : {product.flavor_profile?.join(", ")}</p>
       <p className="px-4 mt-4">{product.description}</p>
+      <Dropdown grindOption={product.grind_option} />
     </div>
   );
 };
